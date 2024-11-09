d327b7b32bdacc79a6e722ce55891e58a31713bb
hiuhuedd <hiuhuedd@gmail.com>
2024-11-09 13:47:29 -0800

    premium2

--- src/app/premium/page.js
+++ src/app/premium/page.js
@@ -1,57 +1,106 @@
 // src/app/premiumPage.js
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function PremiumPage() {
+  const router = useRouter();
   const [selectedPlan, setSelectedPlan] = useState("monthly");
+  const [isLoading, setIsLoading] = useState(true);
+  const [available, setAvailable] = useState(0);
+
+  useEffect(() => {
+        const ar = sessionStorage.getItem('available');
+    setAvailable(ar)
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
   const handlePlanSelect = (plan) => {
     setSelectedPlan(plan);
-    alert(`Selected ${plan === "monthly" ? "Monthly" : "Semester"} Plan`);
+    // alert(`Selected ${plan === "monthly" ? "Monthly" : "Semester"} Plan`);
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4 sm:p-6 font-poppins">
-      <div className="bg-white shadow-lg rounded-lg w-full max-w-lg p-4 sm:p-6 space-y-4 sm:space-y-6 text-center scale-90 sm:scale-100">
-        <h2 className="text-2xl sm:text-3xl font-bold text-teal-800">Go Premium</h2>
-        <p className="text-sm sm:text-base text-gray-700">Get full access to all resources and exclusive content!</p>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          {/* Monthly Plan */}
-          <div
-            className={`p-4 sm:p-6 border-2 rounded-lg cursor-pointer ${
-              selectedPlan === "monthly" ? "border-teal-800" : "border-gray-300"
-            }`}
-            onClick={() => handlePlanSelect("monthly")}
-          >
-            <h3 className="text-lg sm:text-xl font-semibold">Monthly Plan</h3>
-            <p className="mt-1 sm:mt-2 text-xs sm:text-sm text-gray-600">Access for 30 days</p>
-            <p className="mt-3 sm:mt-4 text-xl sm:text-2xl font-bold text-teal-800">Ksh 99</p>
-            <p className="text-xs text-gray-500">Billed monthly</p>
-          </div>
-
-          {/* Semester Plan */}
-          <div
-            className={`p-4 sm:p-6 border-2 rounded-lg cursor-pointer ${
-              selectedPlan === "semester" ? "border-teal-800" : "border-gray-300"
-            }`}
-            onClick={() => handlePlanSelect("semester")}
-          >
-            <h3 className="text-lg sm:text-xl font-semibold">Semester Plan</h3>
-            <p className="mt-1 sm:mt-2 text-xs sm:text-sm text-gray-600">Access for 4 months</p>
-            <p className="mt-3 sm:mt-4 text-xl sm:text-2xl font-bold text-teal-800">Ksh 249</p>
-            <p className="text-xs text-gray-500">Billed once per semester</p>
-          </div>
-        </div>
-
-        <button
-          onClick={() => handlePlanSelect(selectedPlan)}
-          className="w-full bg-teal-800 text-white py-2 sm:py-3 rounded-lg text-sm sm:text-lg font-semibold mt-4"
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4 sm:p-6 font-poppins relative">
+      {/* Back Arrow */}
+      <button
+        onClick={() => router.back()}
+        className="absolute flex top-4 left-4 p-2 text-teal-800 hover:text-teal-600"
+      >
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          Subscribe to {selectedPlan === "monthly" ? "Monthly" : "Semester"} Plan
-        </button>
-        <p className="text-xs sm:text-xs text-gray-400 mt-2">You can cancel anytime in your account settings.</p>
-      </div>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7"></path>
+        </svg>
+        Back
+      </button>
+
+      {isLoading ? (
+        <div className="flex items-center justify-center">
+          <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-teal-800"></div>
+        </div>
+      ) : (
+        <div className="relative bg-white shadow-lg rounded-lg w-full max-w-lg p-4 sm:p-6 space-y-4 sm:space-y-6 text-center scale-90 sm:scale-100 relative">
+            <img
+                src="/xmas.png"
+                alt="Special Offer"
+                className="absolute top-2 right-2 w-16 h-18 sm:w-8 sm:h-8"
+              />
+          
+          <h2 className="text-2xl sm:text-3xl font-bold text-teal-800">Go Premium</h2>
+          <p className="text-sm sm:text-base text-gray-700">
+            Get full access to all {available} resources!
+          </p>
+
+          <div className="  grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {/* Monthly Plan */}
+        
+            <div
+              className={`p-4 sm:p-6 border-2 rounded-lg cursor-pointer ${
+                selectedPlan === "monthly" ? "border-teal-800" : "border-gray-300"
+              } relative`}
+              onClick={() => handlePlanSelect("monthly")}
+            >
+            
+              <h3 className="text-lg sm:text-xl text-gray-600 font-semibold">Monthly Plan</h3>
+              <p className="mt-1 sm:mt-2 text-xs sm:text-sm text-gray-600">Access for 30 days</p>
+              <p className="mt-3 sm:mt-4 text-xl sm:text-2xl font-bold text-teal-800">Ksh 99/=</p>
+              <p className="text-xs text-gray-500">Billed monthly</p>
+            </div>
+
+            {/* Semester Plan */}
+            <div
+              className={`p-4 sm:p-6 border-2 rounded-lg cursor-pointer ${
+                selectedPlan === "semester" ? "border-teal-800" : "border-gray-300"
+              } relative`}
+              onClick={() => handlePlanSelect("semester")}
+            >
+            
+              <h3 className="text-lg sm:text-xl text-gray-600 font-semibold">Semester Plan</h3>
+              <p className="mt-1 sm:mt-2 text-xs sm:text-sm text-gray-600">Access for 4 months</p>
+              <p className="mt-3 sm:mt-4 text-xl sm:text-2xl font-bold text-teal-800">Ksh 249/=</p>
+              <p className="text-xs text-gray-500">Billed once per semester</p>
+            </div>
+          </div>
+
+          <button
+            onClick={() => handlePlanSelect(selectedPlan)}
+            className="w-full bg-teal-800 text-white py-2 sm:py-3 rounded-lg text-sm sm:text-lg font-semibold mt-4"
+          >
+            Subscribe to {selectedPlan === "monthly" ? "Monthly" : "Semester"} Plan
+          </button>
+          <p className="text-xs sm:text-xs text-gray-400 mt-2">
+            You can cancel anytime in your account settings.
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- src/app/resources/page.js
+++ src/app/resources/page.js
@@ -11,16 +11,17 @@ export default function Resources() {
   const [resources, setResources] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [isLoading, setIsLoading] = useState(true);
-  const [isDarkMode, setIsDarkMode] = useState(false); // Add state for dark mode
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const router = useRouter();
 
   useEffect(() => {
     const fetchResources = async () => {
       try {
         const response = await axios.get('https://automated-resource-upload.onrender.com/resources');
         setResources(response.data.reverse());
-        console.log(response.data);
       } catch (error) {
-        alert('Error fetching resources:');
+        alert('Error fetching resources');
         console.error('Error fetching resources:', error);
       } finally {
         setIsLoading(false);
@@ -30,50 +31,48 @@ export default function Resources() {
     fetchResources();
   }, []);
 
-  const filteredResources = resources.filter(resource =>
-    resource.unitName.toLowerCase().includes(searchTerm.toLowerCase())||resource.unitCode.toLowerCase().includes(searchTerm.toLowerCase())
-
+  const filteredResources = resources.filter((resource) =>
+    resource.unitName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    resource.unitCode.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   const toggleTheme = () => {
-    setIsDarkMode((prevMode) => !prevMode); // Toggle between light and dark themes
+    setIsDarkMode((prevMode) => !prevMode);
   };
 
-  const router = useRouter();
-
-
+  const handleViewResource = (resource, index) => {
+    if (index >= 3) {
+      // Redirect to premium page for premium resources
+      sessionStorage.setItem('available', resources.length);
+      router.push('/premium');
+    } else {
+      // Redirect to view page for free resources
+      sessionStorage.setItem('fileURI', resource.fileURI);
+      router.push('/view');
+    }
+  };
 
   return (
-    <>
-
-
     <div className={`min-h-screen p-4 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-gray-200 text-black'} font-poppins`}>
-    <div className="flex flex-col sm:flex-row justify-between items-center mb-4 space-y-2 sm:space-y-0 sm:space-x-4">
-  <input
-    type="text"
-    placeholder="Search by unit name, unit code"
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-    className="p-2 border border-gray-300 rounded text-teal-800 w-full sm:w-auto sm:flex-1"
-  />
-
-  <div className="flex flex-col sm:flex-row items-center space-y-1 sm:space-y-0 sm:space-x-4">
-    <h3 className="font-semibold text-xs">Assets: {filteredResources.length * 2181}</h3>
-    <h3 className="font-semibold text-xs">Downloads: {resources.length * 200}</h3>
-  </div>
-
-  <button
-    onClick={toggleTheme}
-    className="px-2 py-1 bg-blue-500 text-white rounded flex items-center justify-center"
-  >
-    {isDarkMode ? (
-      <SunIcon className="h-5 w-5" />
-    ) : (
-      <MoonIcon className="h-5 w-5" />
-    )}
-  </button>
-</div>
-
+      <div className="flex flex-col sm:flex-row justify-between items-center mb-4 space-y-2 sm:space-y-0 sm:space-x-4">
+        <input
+          type="text"
+          placeholder="Search by unit name, unit code"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="p-2 border border-gray-300 rounded text-teal-800 w-full sm:w-auto sm:flex-1"
+        />
+        <div className="flex flex-col sm:flex-row items-center space-y-1 sm:space-y-0 sm:space-x-4">
+          <h3 className="font-semibold text-xs">Assets: {filteredResources.length }</h3>
+          <h3 className="font-semibold text-xs">Downloads: {resources.length -resources.length/3 }</h3>
+        </div>
+        <button
+          onClick={toggleTheme}
+          className="px-2 py-1 bg-blue-500 text-white rounded flex items-center justify-center"
+        >
+          {isDarkMode ? <SunIcon className="h-5 w-5" /> : <MoonIcon className="h-5 w-5" />}
+        </button>
+      </div>
 
       {isLoading ? (
         <div className="flex justify-center items-center min-h-screen">
@@ -82,58 +81,49 @@ export default function Resources() {
       ) : (
         <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
           {filteredResources.map((resource, index) => (
-            <div key={index} className={`p-6 shadow rounded ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white'} `}>
-
-          <div className={`p-4 rounded flex items-center  `}>
+            <div key={index} className={`p-6 shadow rounded ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-white'}`}>
+              <div className="p-4 rounded flex items-center">
                 <img
-                  src="ku.png" // Reference the image in the public folder
+                  src="ku.png"
                   alt="Resource Image"
-                  className="w-10 h-10 object-cover  rounded-full " // Set a smaller width and height (e.g., 8rem x 8rem)
-                  />
-               <div className={`p-4 rounded `}>
-               <h3 className="font-bo">KU Past Papers</h3>
-               <p className='text-xs'>{moment(resource.resourceDate).format('MMMM Do YYYY, h:mm:ss a')}</p>
-               
+                  className="w-10 h-10 object-cover rounded-full"
+                />
+                <div className="p-4 rounded">
+                  <h3 className="font-bold">KU Past Papers</h3>
+                  <p className="text-xs">{moment(resource.resourceDate).format('MMMM Do YYYY, h:mm:ss a')}</p>
+                </div>
               </div>
-              </div>
-             
               <div className={`py-4 px-1 shadow rounded flex justify-start items-center ${resource.isNotes ? 'bg-teal-800' : 'bg-sky-700'} mb-4`}>
                 <img
-                  src="/pdf3.png" // Reference the image in the public folder
+                  src="/pdf3.png"
                   alt="Resource Image"
-                  className="w-24 h-24 object-cover  p-3 mb-4" // Set a smaller width and height (e.g., 8rem x 8rem)
-                  />
-                <div className={`p-4 rounded  ml-2  mb-4`}>
-                <p className={` uppercase ${isDarkMode ? ' text-gray-100' : 'text-gray-100'}`}> {resource.unitCode}</p>               
-                <h3 className={` uppercase text-sm ${isDarkMode ? ' text-gray-300' : 'text-gray-300'}`}>{resource.isNotes ? 'Notes Available' : 'Past Paper'}</h3>
+                  className="w-24 h-24 object-cover p-3 mb-4"
+                />
+                <div className="p-4 rounded ml-2 mb-4">
+                  <p className={`uppercase ${isDarkMode ? 'text-gray-100' : 'text-gray-100'}`}>{resource.unitCode}</p>
+                  <h3 className={`uppercase text-sm ${isDarkMode ? 'text-gray-300' : 'text-gray-300'}`}>{resource.isNotes ? 'Notes Available' : 'Past Paper'}</h3>
+                </div>
+                <div className="relative w-full h-full">
+                  {index >= 3 && (
+                    <img
+                      src="/premium.png"
+                      alt="Premium Badge"
+                      className="absolute top-8 right-0 w-10 h-12 object-cover p-3 mb-4"
+                    />
+                  )}
+                </div>
               </div>
-              <div className='relative w-full h-full'>
-              {index >= 3 && (
-                      <img
-                        src="/premium.png"
-                        alt="Premium Badge"
-                        className="absolute top-8 right-0 w-10 h-12 object-cover p-3 mb-4"
-                      />
-                    )}
-              </div>
-              </div>
-          
-              <p className='uppercase'>              {resource.unitName}
-              </p>
+              <p className="uppercase">{resource.unitName}</p>
               <button
-                onClick={()=>   {  
-                  sessionStorage.setItem('fileURI', resource.fileURI);
-                  router.push('/premium')   } }
+                onClick={() => handleViewResource(resource, index,)}
                 className="px-2 py-2 mt-3 text-xs bg-sky-800 text-white rounded flex items-center justify-center"
               >
                 View Resource
               </button>
-
             </div>
           ))}
         </div>
       )}
     </div>
-    </>
   );
 }
